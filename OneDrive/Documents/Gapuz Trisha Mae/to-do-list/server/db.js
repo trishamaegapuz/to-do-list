bda8585bc5c0194792fe56fa4139aa542af9a51b
Trisha Mae Gapuz <baldosmeliee@gmail.com>
2026-02-03 21:57:01 +0800

    server

--- OneDrive/Documents/Gapuz Trisha Mae/to-do-list/server/db.js
+++ OneDrive/Documents/Gapuz Trisha Mae/to-do-list/server/db.js
@@ -21,3 +21,5 @@ if (process.env.NODE_ENV === "development") {
     connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?sslmode=require`
   });
 }
+
+export const pool = newPool;
