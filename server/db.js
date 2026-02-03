90f6b5dad38dc52627d8321e133957fd9f60d95d
Trisha Mae Gapuz <baldosmeliee@gmail.com>
2026-02-03 20:00:59 +0800

    server

--- server/db.js
+++ server/db.js
@@ -1,16 +1,25 @@
-// server/db.js
-import pg from "pg";
-const { Pool } = pg;
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-// Idagdag ang ?sslmode=require sa dulo
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?sslmode=require`;
+let newPool;
 
-export const pool = new Pool({
-  connectionString: connectionString,
-  ssl: {
-    rejectUnauthorized: false, // Panatilihin ito para sa self-signed certificates ng cloud providers
-  },
-});
+if (process.env.NODE_ENV === "development") {
+  newPool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT), // ensure number
+  });
+} else {
+  newPool = new Pool({
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}?options=project=${process.env.ENDPOINT_ID}`,
+  });
+}
+
+export const pool = newPool;
