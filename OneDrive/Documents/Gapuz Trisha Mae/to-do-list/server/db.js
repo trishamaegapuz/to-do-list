ee430d5230effc91710e1864a8f9597564a4237a
Trisha Mae Gapuz <baldosmeliee@gmail.com>
2026-02-03 22:27:43 +0800

    server

--- OneDrive/Documents/Gapuz Trisha Mae/to-do-list/server/db.js
+++ OneDrive/Documents/Gapuz Trisha Mae/to-do-list/server/db.js
@@ -1,25 +1,27 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-let newPool;
+let pool;
 
 if (process.env.NODE_ENV === "development") {
-  newPool = new Pool({
+  pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT), // ensure number
+    port: Number(process.env.DB_PORT) || 5432,
   });
 } else {
-  newPool = new Pool({
+  // FIXED: Inilagay ang tamang format para sa cloud deployment
+  pool = new Pool({
+    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?sslmode=require`,
     ssl: {
       rejectUnauthorized: false,
     },
-    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?sslmode=require`
   });
 }
 
-export const pool = newPool;
+export { pool };
