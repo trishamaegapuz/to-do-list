698941cc33c8647058914f960d09de6092b4c797
Trisha Mae Gapuz <baldosmeliee@gmail.com>
2026-02-06 14:37:17 +0800

    Remove OneDrive folder repository

--- OneDrive/Documents/Gapuz Trisha Mae/to-do-list/README.md
+++
@@ -1,6 +0,0 @@
-LOGIN
-![alt text](image.png)
-REGISTER
-![alt text](image-1.png)
-HOME
-![alt text](image-2.png)
--- OneDrive/Documents/Gapuz Trisha Mae/to-do-list/client/src/pages/List-item.jsx
+++
@@ -1,8 +0,0 @@
-function ListItem({ item }) {
-  return (
-    <div className="p-4 border rounded-xl">
-      {item.description}
-    </div>
-  );
-}
-export default ListItem;
